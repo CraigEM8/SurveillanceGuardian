9f3bddf95ebdb54efc92f2ba4a11a383b4d36643
ChrisEngineerM8 <chris@engineerm8.com>
2025-03-07 13:54:42 +0000

    Script update

--- credentials.py
+++ credentials.py
@@ -2,6 +2,6 @@
 ipAddress = "192.168.1.73"
 username = "admin"
 password = "Concept1"
-#db_address = "https://em8database.com/api"
-db_address = "http://127.0.0.1:5000"
+db_address = "https://em8database.com/api"
+#db_address = "http://127.0.0.1:5000"
 iccid = ""
