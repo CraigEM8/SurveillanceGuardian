e9941f4e8de8f7d94c30e755f379eda0f454e6f7
ChrisEngineerM8 <chris@engineerm8.com>
2025-03-10 16:43:27 +0000

    Script update

--- conf.sh
+++ conf.sh
@@ -9,5 +9,9 @@ echo "Installing Minicom..."
 sudo apt install minicom
 echo "Minicom Installed."
 
+wget "https://assist.zoho.eu/api/v2/install_urs?type=2&encapiKey=yA6Kbntb7AT1xTlXFRJr05mC%2B4g4rvhv3inltijnLs10LoG12qFp0BdtdoS9IzfTjY6A460AOo8XJYi%2F6N1fKpE3MtZVKJTGTuv4P2uV48xhudyNcNsuhJyoA7EQEqNLcBIi&app=linux&version=arm64&isDebian=true" -O zohoassist_1.0.0.1.deb
+sudo dpkg -i zohoassist_1.0.0.1.deb
+echo "Zoho Assist Installed."
+
 sudo apt-get update
 sudo apt-get full-upgrade
